8f3014ab7303eabae5bac35472f4f5cc987c573c
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-12-23 15:36:39 -0500

    automatic build of site

--- rmd_posts/2015-01-14-05-Explore.R
+++ rmd_posts/2015-01-14-05-Explore.R
@@ -1,1 +1,21 @@
 
+## ----summary_data_example------------------------------------------------
+summary(iris)
+
+
+## ----range_examp---------------------------------------------------------
+range(iris$Petal.Length)
+
+
+## ----iqr_examp-----------------------------------------------------------
+IQR(iris$Sepal.Width)
+
+
+## ----quantile_example----------------------------------------------------
+quantile(iris$Sepal.Length)
+
+
+## ----quantile_probs_examp------------------------------------------------
+quantile(iris$Sepal.Length, probs=(c(0.025,0.975)))
+
+
