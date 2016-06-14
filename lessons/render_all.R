2ad20054f302c61025b4bd1eccd0983d6d965d68
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-06-14 13:36:13 -0400

    new render all and update on 02

--- lessons/render_all.R
+++ lessons/render_all.R
@@ -5,7 +5,9 @@ render_all <- function(dir = "."){
   rmds <- list.files(dir, ".Rmd",full.names = TRUE)
   for(i in rmds){
     out_i <- gsub(".Rmd",".md",i)
-    knitr::knit(i,out_i)
-    rmarkdown::render(i,"pdf_document")
+    if(file.info(i)$mtime > file.info(out_i)$mtime){
+      knitr::knit(i,out_i)
+      rmarkdown::render(i,"pdf_document")
+    }
   }
 }
