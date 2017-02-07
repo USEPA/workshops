06f23d8b85cf222ecd09d0f9cfdbdf1ad25da11e
Jeff Hollister <jeff.w.hollister@gmail.com>
2017-02-07 15:26:08 -0500

    updates and new renders - through data agg

--- lessons/render_all.R
+++ lessons/render_all.R
@@ -2,6 +2,7 @@ files <- list.files(pattern = "*.Rmd")
 for(i in files){
   rmd_time <- file.info(i)
   md_time <- file.info(gsub(".Rmd",".md",i))
+  if(is.na(md_time$mtime)) {md_time$mtime <- 0}
   if(md_time$mtime <= rmd_time$mtime){
     knitr::knit(i)
     rmarkdown::render(i,output_format = "pdf_document")
