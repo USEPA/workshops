3284421bf1fe46120c2e0f297a23315c9e816c9a
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-11-26 13:43:31 -0500

    cleaned up data for RR

--- R/get_nla_data.R
+++ R/get_nla_data.R
@@ -1,6 +1,6 @@
 library(readr)
 library(dplyr)
-library(httr)
+library(lubridate)
 
 nla_2007_site_url <- "https://www.epa.gov/sites/production/files/2014-01/nla2007_sampledlakeinformation_20091113.csv"
 nla_2007_wc_url <- "https://www.epa.gov/sites/production/files/2014-10/nla2007_chemical_conditionestimates_20091123.csv"
@@ -17,3 +17,26 @@ nla_2007_secchi <- read_csv(nla_2007_secchi_url)
 nla_2012_site <- read_csv(nla_2012_site_url)
 nla_2012_key_var <- read_csv(nla_2012_key_var_url)
 nla_2012_secchi <- read_csv(nla_2012_secchi_url)
+
+nla_2007 <- nla_2007_site %>%
+  left_join(nla_2007_wc, by = c("SITE_ID", "VISIT_NO", "LON_DD", "LAT_DD", "ST")) %>%
+  left_join(nla_2007_secchi, by = c("SITE_ID", "VISIT_NO")) %>%
+  rename_all(tolower) %>%
+  select(site_id, visit_no, date_col, lon_dd, lat_dd, st, cntyname, ptl,
+         ntl, chla, secchi = secmean) %>%
+  filter(!is.na(chla)) %>%
+  mutate(date_col = mdy(date_col))
+  
+write_csv(nla_2007, "data/nla_2007.csv")
+
+nla_2012 <- nla_2012_site %>%
+  left_join(nla_2012_key_var, by = c("SITE_ID", "VISIT_NO")) %>%
+  left_join(nla_2012_secchi, by = c("SITE_ID", "VISIT_NO", "DATE_COL")) %>%
+  rename_all(tolower) %>%
+  select(site_id, visit_no, date_col,lon_dd = lon_dd83, lat_dd = lat_dd83, 
+         state, cntyname, ptl = ptl_result, ntl = ntl_result, 
+         chla = chlx_result, secchi) %>%
+  filter(!is.na(chla)) %>%
+  mutate(date_col = mdy(date_col))
+
+write_csv(nla_2012, "data/nla_2012.csv")
